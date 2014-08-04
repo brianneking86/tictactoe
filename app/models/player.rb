6e0ff48f6cec0d98ddd59d245c4bacad02396827
Brianne <brianneking86@gmail.com>
2014-08-04 17:16:35 -0400

    Initial commit of trial TicTacToe

---
+++ app/models/player.rb
@@ -0,0 +1,57 @@
+class Player
+
+	#iniitiate variables to blank for a fresh game
+	def button1
+		button1 = ' '
+	end
+	
+	def button2 
+		button2 = ' '
+	end
+
+	def button3
+		button3 = ' '
+	end
+
+	def button4
+		button4 = ' '
+	end
+
+	def button5
+		button5 = ' '
+	end
+
+	def button6 
+		button6 = ' '
+	end
+
+	def button7
+		button7 = ' '
+	end
+
+	def button8
+		button8 = ' '
+	end
+
+	def button9
+		button9 = ' '
+	end
+
+	#sets date for bottom of screen
+	def pickdate
+		today = Date.today
+	end
+
+	#resets for new game
+
+
+
+
+
+
+
+
+
+
+	
+end
